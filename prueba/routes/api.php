c298ee795ca47108875fc6e11f6089f45454ac21
Sebastian <sebastian.morales@epn.edu.ec>
2020-07-09 16:20:07 -0500

    #5 creacion de rutas y controladores

--- prueba/routes/api.php
+++ prueba/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Customer;
+use App\Product;
 use Illuminate\Http\Request;
 
 /*
@@ -13,6 +15,26 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::get('product', 'ProductController@index');
+Route::get('product/{product}', 'ProductController@show');
+Route::post('product', 'ProductController@store');
+Route::put('product/{product}', 'ProductController@update');
+Route::delete('product/{id}', 'ProductController@delete');
+
+//customers
+Route::get('customer', 'CustomerController@index');
+Route::get('customer/{id}', 'CustomerController@show');
+Route::post('customer', 'CustomerController@store');
+Route::put('customer/{id}', 'CustomerController@update');
+Route::delete('customer/{id}', 'CustomerController@delete');
+
+//users
+Route::get('user', 'UserController@index');
+Route::get('user/{id}', 'UserController@show');
+Route::post('user', 'UserController@store');
+Route::put('user/{id}', 'UserController@update');
+Route::delete('user/{id}', 'UserController@delete');
